df482c53650653e066f1c2181635991583eb44e3
andre.santos <alssant@gmail.com>
2026-05-31 17:56:04 -0300

    test: configure postgres testcontainer

--- src/test/java/com/alssant/flyway_testcontainers/FlywayTestcontainersApplicationTests.java
+++ src/test/java/com/alssant/flyway_testcontainers/FlywayTestcontainersApplicationTests.java
@@ -1,15 +1,31 @@
 package com.alssant.flyway_testcontainers;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class FlywayTestcontainersApplicationTests {
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void shouldConnectToPostgres() throws Exception{
+		try(Connection connection = dataSource.getConnection()) {
+			Assertions.assertFalse(connection.isClosed());
+		}
+	}
+
+
 
 }
